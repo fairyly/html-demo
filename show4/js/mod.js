d96bbae6a64c6ea701d7c9920d0729f11c371995
fairyly <498745097@qq.com>
2017-02-21 16:33:21 +0800

    add user-center.html

---
+++ show4/js/mod.js
@@ -0,0 +1,12 @@
+function C() {
+  var sum = 0;
+  // this.add = function () {
+  //   this.sum += 1;
+  // };
+  // this.show = function () {
+  //   console.log(this.sum);
+  // };
+  console.log(sum);
+}
+
+export let c = new C();
